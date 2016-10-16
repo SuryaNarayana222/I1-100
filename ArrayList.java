63662347c6d9f88e0b714b0d628b1d305269f7cd
SuryaNarayana222 <suryaece402@gmail.com>
2016-10-16 11:58:50 +0530

    Test

---
+++ ArrayList.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ArrayListDemo
+{
+public static void main (String args[]){
+ArrayList l= null;
+ArrayList j= null;
+
+ l = new ArrayList();
+ j = new ArrayList();
+l.add(12);
+l.add(1);
+l.add(32);
+l.add(3);
+System.out.println(l);
+j.add(0);
+j.add(12);
+j.add(2);
+j.add(3);
+System.out.println(j);
+}
+}
---
+++ Harsh.java
@@ -0,0 +1,6 @@
+public class Harsh{
+public static void main(String[] args){
+Student s= new Student();
+System.out.println("int id="+s.id);
+}
+}
